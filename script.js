a4ae5c4ae0a4622ec8acd348f826f91a13acd575
sorna <sorna@gmail.com>
2023-09-04 19:19:37 +0530

    first commit

--- script.js
+++ script.js
@@ -1,44 +1,78 @@
+//Elements to Access
 let input = document.getElementById('input');
 let button = document.getElementById('add');
-let todolist = document.getElementById('todolist');
+let taskName=document.querySelector(".taskName");
 
-
+//arrays
 let todos=[];
-
-//button.addEventListener("dblclick",(e)=>{console.log('Double-click works')});
-//input.addEventListener('dblclick',()=>{console.log('Hello')});
-['click','keypress'].forEach(e=>
-button.addEventListener(e,()=>{
-let todo=input.value;
-todos.push(todo);
-console.log(todos);
-input.value='';
-
-
-
-let txt=document.createElement('p');
-txt.innerText=todo;
-todolist.appendChild(txt);
-
-
-txt.addEventListener('click',()=>{
-    txt.style.textDecoration = 'line-through';
-})
-
-txt.addEventListener('dblclick',()=>{
-    let ind=todos.indexOf(todo);
-    console.log(todo);
-    console.log(ind);
-    if(ind>-1){
-    todos.splice(ind,1);
-    }
-    console.log(todos);
-
-    todolist.removeChild(txt);
-    })
-
-
-
+window.onload=()=>{
+    todos=JSON.parse(localStorage.getItem('prevTodos'))||[];
+    todos.forEach(todo=>
+        {
+            input.value=todo;
+            addArray();
+});
 }
-)
-);
+
+
+//Event-Listeners
+button.addEventListener('click',addArray);
+
+//functions
+
+function addArray(){
+    if(input.value!='')
+    {
+    todos.push(input.value);
+    console.log(todos);
+    displayTodo();
+    }
+}
+
+function displayTodo(){
+    if(input.value!='')
+    {
+    
+    document.querySelector('#todolist').innerHTML+=
+    `<div class="newTask">
+     <p class="taskName" onclick="strikeOff(this)">
+     ${input.value}
+     </p>
+     <button class="delete" onclick="removeTodo(this)" >
+      X
+     </button>
+    
+    </div>`
+    
+    }
+    input.value='';
+}
+
+function strikeOff(clickedTodo)
+{
+    clickedTodo.style="text-decoration:line-through;"
+}
+
+ function removeTodo(btn)
+ {
+    let parent=((btn.parentNode).parentNode);
+    parent.removeChild(btn.parentNode);
+    let divParent=btn.parentNode;
+    let todo=divParent.querySelector('.taskName');
+    removeFromArray(todo.innerHTML);
+
+ }
+
+ function removeFromArray(todo)
+ {
+    let stringTodo=todo.toString().trim();
+    console.log(stringTodo);
+   
+    let x=todos.indexOf(stringTodo);
+    console.log(x);
+    if(x>-1)
+    todos.splice(x,1);
+    console.log(todos);
+    localStorage.setItem('prevTodos',JSON.stringify(todos));
+ }
+
